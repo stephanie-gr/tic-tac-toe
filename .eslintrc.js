dba565ce659f1c0fde98f895dbb07ca722067a31
Stephanie Grant <stephanie@Stephanies-MacBook-Air.local>
2021-11-03 13:34:41 -0700

    eslint/prettier setup

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint",
+        "prettier/react"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    "rules": {
+    }
+};
