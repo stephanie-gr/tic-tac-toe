c6f811b43dc743cfedec77dd90984299385e6375
Stephanie Grant <stephanie@Stephanies-MacBook-Air.local>
2021-11-04 14:31:29 -0700

    setting up file structure

---
+++ src/screens/game/game.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import style from "./game.styles";
+
+export default function Game() {
+  return (
+    <View style={style.container}>
+      <Text>Game</Text> 
+    </View>
+  )
+}
