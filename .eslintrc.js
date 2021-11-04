61b97d20499b8ad9e9f17f758301f26e4c34d7b6
Stephanie Grant <stephanie@Stephanies-MacBook-Air.local>
2021-11-04 12:23:45 -0700

    husky and lint-staged setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,27 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-        "prettier/@typescript-eslint",
-        "prettier/react"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  settings: {
+    react: {
+      version: "detect",
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    settings: {
-      react: {
-        version: "detect"
-      }
-    },
-    "rules": {
-    }
+  },
+  rules: {},
 };
